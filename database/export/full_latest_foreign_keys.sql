001cf5ccae036c92717f7156905a0db2dc2f748c
MurLoper <13451101947@163.com>
2026-04-11 22:41:32 +0800

    最新数据同步

--- database/export/full_latest_foreign_keys.sql
+++ database/export/full_latest_foreign_keys.sql
@@ -1,5 +1,5 @@
 -- StructSim FK Restore SQL
--- ExportedAt: 2026-04-11T00:04:19.309382
+-- ExportedAt: 2026-04-11T22:03:40.045868
 
 SET FOREIGN_KEY_CHECKS=0;
 ALTER TABLE `users` ADD CONSTRAINT `fk_users_department_id` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`);
